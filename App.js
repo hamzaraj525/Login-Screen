3b7c16d82aea6b615fe8a6b80811d77a05f6289d
M HAMZA <hamzarajput553@gmail.com>
2021-06-09 20:46:48 -0700

    validations applied

--- App.js
+++ App.js
@@ -1,59 +1,104 @@
-import * as React from 'react';
+import React, {Component} from 'react';
 import {Appbar} from 'react-native-paper';
 import {
   SafeAreaView,
-  Alert,
   TouchableOpacity,
   ScrollView,
   Button,
   TextInput,
   StyleSheet,
   Text,
+  Alert,
   useColorScheme,
   View,
 } from 'react-native';
 
-const App = () => {
-  const [number, onChangeNumber] = React.useState(null);
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      nameValidate: true,
+      passwordValidate: true,
+      password: '',
+    };
+  }
 
-  return (
-    <View>
-      <Appbar.Header style={styles.header}>
-        <Appbar.Content title="Login" />
-      </Appbar.Header>
+  validate(text, type) {
+    alph = /^[a-zA-Z]+$/;
+    num = /^[0-9]+$/;
+    if (type == 'username') {
+      if (alph.test(text)) {
+        this.setState({
+          nameValidate: true,
+        });
+      } else {
+        this.setState({
+          nameValidate: false,
+        });
+      }
+    } else if (type == 'password') {
+      if (num.test(text)) {
+        this.setState({
+          passwordValidate: true,
+        });
+      } else {
+        this.setState({
+          passwordValidate: false,
+        });
+      }
+    }
+  }
+  render() {
+    return (
+      <View>
+        <Appbar.Header style={styles.header}>
+          <Appbar.Content title="Login" />
+        </Appbar.Header>
 
-      <View style={styles.container}>
-        <Text style={styles.heading}>Login Screen</Text>
-        <TextInput style={styles.input} placeholder="Username" />
-        <TextInput
-          style={styles.input}
-          placeholder="Password"
-          returnKeyType="go"
-          secureTextEntry
-          autoCorrect={false}
-        />
-        <View
-          style={[
-            {
-              width: '100%',
-              alignItems: 'center',
-              marginTop: 6,
-            },
-          ]}>
-          <Button
-            title="Login"
-            onPress={() => Alert.alert('Login Button pressed')}
+        <View style={styles.container}>
+          <Text style={styles.heading}>Login Screen</Text>
+          <TextInput
+            style={[
+              styles.input,
+              !this.state.nameValidate ? styles.error : null,
+            ]}
+            onChangeText={text => this.validate(text, 'username')}
+            placeholder="Username"
           />
-          <Text style={styles.or}>OR</Text>
-          <Button
-            title="Register"
-            onPress={() => Alert.alert('Registeration Button pressed')}
+          <TextInput
+            style={[
+              styles.input,
+              !this.state.passwordValidate ? styles.error : null,
+            ]}
+            onChangeText={text => this.validate(text, 'password')}
+            placeholder="Password"
+            secureTextEntry
+            autoCorrect={false}
           />
+          <View
+            style={[
+              {
+                width: '100%',
+                alignItems: 'center',
+                marginTop: 6,
+              },
+            ]}>
+            <Button
+              title="Login"
+              onPress={() => Alert.alert('Login Button pressed')}
+            />
+            <Text style={styles.or}>OR</Text>
+            <Button
+              title="Register"
+              onPress={() => Alert.alert('Registeration Button pressed')}
+            />
+          </View>
         </View>
       </View>
-    </View>
-  );
-};
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   input: {
@@ -85,5 +130,8 @@ const styles = StyleSheet.create({
     padding: 19,
     marginTop: 133,
   },
+  error: {
+    borderWidth: 4,
+    borderColor: 'red',
+  },
 });
-export default App;
